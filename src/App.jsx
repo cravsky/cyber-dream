7fbf4e0c351be08d64f89860a5bf80733b38cd6e
cravsky <cravsky@gmail.com>
2024-09-13 09:28:20 +0200

    Created initial page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-import { useState } from 'react'
+import Form from './components/Form/Form'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [text, setText] = useState('')
-  const [response, setResponse] = useState('')
-
-  const getInterpretation = async () => {
-    const response = await fetch('http://localhost:8000/interpret', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', },
-      body: JSON.stringify({ text }),
-    })
-      const data = await response.json()
-      console.log(data);
-      setResponse(data.response);
-
-  }
-
   return (
-    <div>
-      <h1>Enter your dream</h1> 
-      <p>Enter your dream and AI will interpret it for you</p>
-      <textarea onChange={e => setText(e.target.value)} />
-      <button onClick={getInterpretation}>Submit</button>
-      <p>{response}</p>
-    </div>
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+export default function Form() {
+
+    const [text, setText] = useState('')
+    const [response, setResponse] = useState('')
+
+    const getInterpretation = async () => {
+        const response = await fetch('http://localhost:8000/interpret', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({ text }),
+        })
+        const data = await response.json()
+        console.log(data);
+        setResponse(data.response);
+
+    }
+
+    return (
+        <div>
+            <h1>Enter your dream</h1>
+            <p>Enter your dream and AI will interpret it for you</p>
+            <textarea onChange={e => setText(e.target.value)} />
+            <button onClick={getInterpretation}>Submit</button>
+            <p>{response}</p>
+        </div>
+    )
+
+}
