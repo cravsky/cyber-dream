a885714e63f1d0d33cedc4afd7eb8b3b3ce6f101
cravsky <cravsky@gmail.com>
2025-03-21 09:22:08 +0100

    Added AI images to testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -6,19 +6,19 @@ export default function Testimonials() {
         {
             name: "Synaps AI 1",
             // role: "Psycholog",
-            image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400",
+            image: "/src/assets/ai1.jpg",
             text: "Niesamowita aplikacja! Interpretacje snów są trafne i pełne głębi."
         },
         {
             name: "Synaps AI 2",
             // role: "Terapeuta",
-            image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400",
+            image: "/src/assets/ai2.jpg",
             text: "Uwielbiam prostotę i elegancję tej aplikacji! Analiza snów nigdy nie była tak szybka i łatwa."
         },
         {
             name: "Synaps AI 3",
             // role: "Coach",
-            image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=400",
+            image: "/src/assets/ai3.jpg",
             text: "sennik.dev to piękna i intuicyjna aplikacja, która zaskoczyła mnie dokładnością interpretacji. Polecam każdemu!"
         }
     ];
