d4c7a7ead8a63cee0e42b3221fd8096e1a6e1fac
cravsky <cravsky@gmail.com>
2025-03-25 21:29:25 +0100

    Added version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
+import packageJson from '../../../package.json';
 
 export default function Footer() {
   return (
     <footer className={styles.container}>
       <div className={styles.content}>
-        <p className={styles.copyright}>&copy; 2025 sennik.dev</p>
+        <div className={styles.info}>
+          <p className={styles.copyright}>&copy; 2025 sennik.dev</p>
+          <p className={styles.version}>v{packageJson.version}</p>
+        </div>
         <nav className={styles.links}>
           <Link to="/terms">Regulamin</Link>
           <span className={styles.divider}>|</span>
