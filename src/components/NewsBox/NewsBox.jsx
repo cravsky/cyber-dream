4e56f6c56417300727d423901f73cda7285badb3
cravsky <cravsky@gmail.com>
2025-04-14 23:49:51 +0200

    Updated NewBox

--- src/components/NewsBox/NewsBox.jsx
+++ src/components/NewsBox/NewsBox.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './NewsBox.module.css';
 
-const NEWS_VERSION = '2025-04-14';
+const NEWS_VERSION = '2025-04-14b';
 
 export default function NewsBox() {
   const [showBox, setShowBox] = useState(false);
@@ -29,7 +29,7 @@ export default function NewsBox() {
         <h2 className={styles.heading}>Beta Testy 14.04.2025</h2>
         <ul className={styles.newsList}>
           <li>🧪 Analizy snów dostępne są za <b>darmo!</b></li>
-          <li>🎤 Już wkrótce możliwość korzystania z mikrofonu</li>
+          <li>Już wkrótce: przykładowa analiza snu</li>
         </ul>
         <h3>Podziel się opinią :)</h3>
         <button onClick={handleClose} className={styles.button}>
